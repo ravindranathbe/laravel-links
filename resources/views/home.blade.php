074907579e94e00e5933a4d6669134babac07e33
Ravindranath <ravindranathbe@gmail.com>
2016-10-24 14:48:39 +0530

    updated gulp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
+                <div class="panel-body dashboard-body">
 
 <example></example>
 
